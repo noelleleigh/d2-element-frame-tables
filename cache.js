d19cdb26704f59cb06cf0ddc989d1c7b489c9ea1
Noelle Leigh <noelle_leigh@fastmail.com>
2022-03-23 20:14:26 -0400

    Create the cache dir if it doesn't exist

--- cache.js
+++ cache.js
@@ -4,23 +4,24 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const currentDir = path.dirname(fileURLToPath(import.meta.url));
+const cacheDir = path.join(currentDir, ".cache");
 
 const md5 = (data) => {
   return crypto.createHash("md5").update(data).digest("hex");
 };
 
 const cacheFilePath = (key) => {
-  return path.join(currentDir, ".cache", `${md5(key)}.json`);
+  return path.join(cacheDir, `${md5(key)}.json`);
 };
 
-const cacheObject = (key, data) => {
+const cacheObject = async (key, data) => {
+  await fs.mkdir(cacheDir, { recursive: true });
   return fs.writeFile(cacheFilePath(key), JSON.stringify(data));
 };
 
-const readFromCache = (key) => {
-  return fs
-    .readFile(cacheFilePath(key), "utf-8")
-    .then((contents) => JSON.parse(contents));
+const readFromCache = async (key) => {
+  const contents = await fs.readFile(cacheFilePath(key), "utf-8");
+  return JSON.parse(contents);
 };
 
 export { cacheObject, readFromCache };
